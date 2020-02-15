2de6b9970ca907447331852682c429ba81571719
Mylab <labs@onmycrowd.com>
2020-02-14 17:32:38 -0700

    Refactoring test and initializing react-ga

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,6 +1,7 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import ReactGA from 'react-ga'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-ReactGA.initialize('foo', { testMode: true });
+ReactGA.initialize('foo', { testMode: true, debug: true });
--- test/unit/common/GoogleAnalytics.spec.js
+++ test/unit/common/GoogleAnalytics.spec.js
@@ -1,19 +1,16 @@
 import React from 'react'
+import { fireEvent, render } from 'react-testing-library'
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, render } from '@testing-library/react'
 import ReactGA from 'react-ga'
 
 describe('analytics', () => {
     beforeEach(() => {
         ReactGA.testModeAPI.resetCalls();
-    });
+    })
 
     it('correctly sends analytics', () => {
-        ReactGA.ga('send', 'pageview', '/mypage')
+        ReactGA.pageview('/mypage')
 
-        expect(ReactGA.testModeAPI.calls).toEqual([
-            ['create', 'foo', 'auto'],
-            ['send', 'pageview', '/mypage']
-        ]);
-      });
+        expect(ReactGA.testModeAPI.calls).toEqual([["send", {"hitType": "pageview", "page": "/mypage"}]])
+    })
 })
